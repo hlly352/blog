cb9b4bae91afa93cd8c1d9326b958e7204b31050
unknown <hlly352@gmail.com>
2019-01-28 16:47:19 +0800

    bug_fix

--- resources/views/home/remind.blade.php
+++ resources/views/home/remind.blade.php
@@ -9,7 +9,7 @@
 		尊敬的{{$uname['username']}}:
 
        	您好，感谢您的注册 。这是一封注册确认邮件。 
-		请点击以下链接完成确认： <a href="http://llhmm.cn/home/remind?id={{$id}}&token={{$token}}">http://llhmm.cn/home/remind?id={{$id}}&token={{$token}}</a>
+		请点击以下链接完成确认： <a href="http://l.cn/home/remind?id={{$id}}&token={{$token}}">http://l.cn/home/remind?id={{$id}}&token={{$token}}</a>
 		如果链接不能点击，请复制地址到浏览器，然后直接打开。 
 													
 													二郎巷博客
