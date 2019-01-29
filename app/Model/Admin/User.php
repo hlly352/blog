f491d4c925a82dd921ecda0e395c44465cdf020b
cccjock <1270420623@qq.com>
2019-01-29 11:25:15 +0800

    user.php

--- app/Model/Admin/User.php
+++ app/Model/Admin/User.php
@@ -6,39 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class User extends Model
 {
-    /**
-     * 与模型关联的数据表
-     *
-<<<<<<< HEAD
+   
+    
 
-     * @var string
-     */
-    protected $table = 'user';
-
-     * @var string
-     */
+     
     protected $table = 'user';
 
     protected $primarykey = 'id';
 
-    /**
-     * 该模型是否被自动维护时间戳
-     *
-     * @var bool
-     */
+    
     public $timestamps = false;
 
-    /**
-	 * 不可被批量赋值的属性。
-	 *
-	 * @var array
-	 */
 	protected $guarded = [];
 
 
-    /**
-     * 获得与用户表关联的详情信息 
-     */
+    
     public function infos()
     {
         return $this->hasOne('App\Model\Admin\Userinfo','uid','id');
