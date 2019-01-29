4bec61f6937205a475c2b7164bf7eb03e6726476
unknown <hlly352@gmail.com>
2019-01-29 11:35:40 +0800

    user.php

--- app/Model/Admin/User.php
+++ app/Model/Admin/User.php
@@ -9,30 +9,18 @@ class User extends Model
     /**
      * 与模型关联的数据表
      *
-<<<<<<< HEAD
+
 
      * @var string
      */
     protected $table = 'user';
 
-     * @var string
-     */
-    protected $table = 'user';
 
     protected $primarykey = 'id';
 
-    /**
-     * 该模型是否被自动维护时间戳
-     *
-     * @var bool
-     */
     public $timestamps = false;
 
-    /**
-	 * 不可被批量赋值的属性。
-	 *
-	 * @var array
-	 */
+
 	protected $guarded = [];
 
 
